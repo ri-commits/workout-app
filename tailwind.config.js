52bca34ca8ef873e020e57b263ad9091217bffcc
ri-commits <kartashourychard@gmail.com>
2023-03-25 17:48:58 +0300

    style added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+      russo: ["Russo One", "sans-serif"],
+      archivo: ["Archivo Black", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
